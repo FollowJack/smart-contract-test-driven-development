d728e9b6d578f389616501e8afe9299b0275b644
Deniel Horvatic <deniel.horvatic@porschedigitallab.com>
2018-02-13 12:31:46 +0100

    Wrote test for containing array of strings

--- contracts/VehicleRegistry.sol
+++ contracts/VehicleRegistry.sol
@@ -1,14 +1,19 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.18;
 
 contract VehicleRegistry {
 
     string message = "Hello Vehicle Registry!";
+    bytes32[] vehicles;
 
-    function VehicleRegistry() {
-        // constructor
+    function VehicleRegistry() public {
+        vehicles = new bytes32[](10);
     }
 
-    function GetMessage() returns (string) {
+    function getMessage() public view returns (string) {
         return message;
+    }
+
+    function getVehicles() public constant returns (bytes32[]) {
+        return vehicles;
     }
 }
--- test/register.test.js
+++ test/register.test.js
@@ -1,14 +1,35 @@
 // Specifically request an abstraction for VehicleRegister
 var VehicleRegister = artifacts.require("VehicleRegistry.sol");
 
-contract('VehicleRegister', function(accounts) {
-    it('should return a correct string', function(done) {
+contract('VehicleRegister', function (accounts) {
+    it('Test: (TODO: remove it later) should return a correct string', function (done) {
         var vehicleRegister = VehicleRegister.deployed();
-        vehicleRegister.then(function(contract){
-            return contract.GetMessage.call(); // **IMPORTANT
-        }).then(function(result){
+        vehicleRegister.then(function (contract) {
+            return contract.getMessage.call(); // **IMPORTANT
+        }).then(function (result) {
             assert.isTrue(result === 'Hello Vehicle Registry!');
             done();
         })
     });
+
+    //TODO get array of vehicles
+    it('should contain an array of vehicles', function (done) {
+        var vehicleRegister = VehicleRegister.deployed();
+        vehicleRegister.then(function (contract) {
+            return contract.getVehicles.call(); // **IMPORTANT
+        }).then(function (result) {
+            assert.isTrue(Array.isArray(result));
+            done();
+        })
+    });
+
+    //TODO write test for adding a vehicle
+
+    //TODO write test for removing a vehicle
+
+    //TODO write test for adding a vehicle
+
+
+
+
 });
